18b502bf42a110982c7ee7e048b670a24658f336
ramirotapias <ramiro.tapias.mt@usa.edu.co>
2022-09-14 15:13:05 -0500

    Agregando Ejercicio3.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author David
+ */
+public class Ejercicio3 {
+	public static void main (String []args){
+		System.out.println("Ejercicio 3 Hola mundo");
+}
+}
