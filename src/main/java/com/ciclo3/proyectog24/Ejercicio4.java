f959d96fa25503df7889f816553594c960c96108
ramirotapias <ramiro.tapias.mt@usa.edu.co>
2022-09-14 18:39:12 -0500

    Agregando clase Ejercicio4.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio4.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author David
+ */
+public class Ejercicio4 {
+	
+}
